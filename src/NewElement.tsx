111dfe873cc0b84061aee48be4be5961626b3df7
hardaira <hardaira@yahoo.com>
2025-01-29 14:19:04 +0100

    add solution

--- src/NewElement.tsx
+++ src/NewElement.tsx
@@ -10,7 +10,7 @@ export class NewElement extends React.Component<Props, State> {
   };
 
   handleKeyPress = (e: KeyboardEvent) => {
-    this.setState({ pressedKey: `Last pressed key is ${e.key}` });
+    this.setState({ pressedKey: `Last pressed key is [${e.key}]` });
   };
 
   componentDidMount() {
@@ -26,7 +26,7 @@ export class NewElement extends React.Component<Props, State> {
 
     return (
       <div className="App">
-        <p className="App__message"> [{pressedKey}] </p>
+        <p className="App__message"> {pressedKey} </p>
       </div>
     );
   }
